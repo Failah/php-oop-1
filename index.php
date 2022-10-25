9480d2a4f545b743c38f73e23b9ae2db32b64229
ValerioScharizza <valerio.schiarizza@live.it>
2022-10-25 13:47:35 +0200

    create class Movie with some instances and methods

--- index.php
+++ index.php
@@ -20,10 +20,40 @@ metodi d'istanza che abbiamo visto stamattina e create un file index.php in cui:
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <title>php-oop-1-Valerio</title>
 </head>
 
 <body>
+
+    <?php
+    class Movie
+    {
+        public $title;
+        public $year;
+        public $genre;
+        public $poster;
+        public $synopsis;
+
+        function __construct($_title, $_genre, $_synopsis)
+        {
+            $this->title = $_title;
+            $this->genre = $_genre;
+            $this->synopsis = $_synopsis;
+        }
+
+        public function something()
+        {
+        }
+
+        public function __set($name, $value)
+        {
+            throw new Exception("Cannot add new property \$$name to instance of " . __CLASS__);
+        }
+    }
+
+
+    ?>
 
 </body>
 
